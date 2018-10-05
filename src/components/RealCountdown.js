ef60a3152ea539840ed175cb797d53c1700c3f30
Chris Chatroux <icecriss@hotmail.com>
2018-10-05 14:47:05 +0200

    COMING SOON SECTION ADDED:
    Components:
    - CountdownSection
    - RealCountdown
    - Youtube
    
    Libraries:
    - React Youtube
    - React-Countdown-Now
    
    File Change:
    App.css deleted

---
+++ src/components/RealCountdown.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Countdown from 'react-countdown-now';
+
+// Random component
+const Completionist = () => <span>Already on Screen!</span>;
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    let dayOrDays = "";
+
+    if (days === 0) {
+        days = "";
+    } else if (days <= 1) {
+        dayOrDays = "day";
+    } else {
+        dayOrDays = "days";
+    }
+
+    if (completed) {
+        // Render a complete state
+        return <Completionist />;
+    } else {
+        // Render a countdown
+        return (
+            <div>
+                <p>{days} {dayOrDays}</p>
+                <p>{hours}:{minutes}:{seconds}</p>
+            </div>
+        );
+    }
+};
+
+class RealCountown extends Component {
+    render() {
+        return (
+            <div className="clock">
+                <Countdown date={new Date("Nov 14, 2018 09:00:00")}
+                    renderer={renderer}
+                />
+            </div>
+        );
+    }
+}
+
+export default RealCountown;
