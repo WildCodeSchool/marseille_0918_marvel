a388ff3f7ddaae39ccbe30ea6ef31025a17a3369
Chris Chatroux <icecriss@hotmail.com>
2018-10-17 19:55:29 +0200

    MODIFICATION OF COMING SOON SECTION
    1- CSS grid added
    2.1- For 768px and above : the countdown is on the right of the video
    2.2- For under 768px : the countdown is under the video

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import './App.scss';
-import NavBar from './components/NavBar/NavBar';
-import Slide from './components/Slide/Slide';
-import Menu from './components/Menu/menu';
-import Footer from './components/Footer/Footer';
+import React, { Component } from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
+import Slide from "./components/Slide/Slide";
+import Menu from "./components/Menu/menu";
+import CountdownSection from "./components/Countdown/CountdownSection";
+import Footer from "./components/Footer/Footer";
 
-import Background from './bg.jpg';
+import Background from "./bg.jpg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <NavBar/>
-          <Slide/>
+          <NavBar />
+          <Slide />
         </header>
         <Menu />
+        <section className="Countdown">
+          <CountdownSection />
+        </section>
         <Footer />
       </div>
     );
