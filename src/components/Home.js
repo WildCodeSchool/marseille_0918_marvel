1fda89458ccb0c85f3021194247e78d1ffd2e4e6
Maxime Pochet <maxx.iime.p@gmail.com>
2018-10-19 23:48:35 +0200

    ADDED VARIABLES AND FIX PROBLEMES :
    Fix search on Hero Page
    
    Add fonts and colors on variables.scss
    
    Import Variables and change name of colors and fonts in my scss files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../App.scss";
-import NavBar from "./NavBar/NavBar";
+import NavBar from './NavBar/NavBar'
 import Slide from "./Slide/Slide";
 import Menu from "./Menu/menu";
 import CountdownSection from "./Countdown/CountdownSection";
