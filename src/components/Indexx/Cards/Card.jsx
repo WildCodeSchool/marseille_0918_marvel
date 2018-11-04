89242d7221e37623c409f372b8f5d6022c7bf8ff
Chris Chatroux <icecriss@hotmail.com>
2018-11-04 20:54:11 +0100

    i kinda sorta forgot to link the index card the hero page

--- src/components/Indexx/Cards/Card.jsx
+++ src/components/Indexx/Cards/Card.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-
-const Card = ({name, comics, image}) => {
-    return (
-        <div className="card">
-            <a href="#">
-                <div className="front">
-                    <img src={image} alt={name}/>
-                </div>
-                <div className="back">
-                    <div className="details">
-                        <h2>{name}<br /><span>{comics}</span></h2>
-                    </div>
-                </div>
-            </a>
+const Card = ({ name, comics, image, identification }) => {
+  return (
+    <div className="card">
+      <NavLink
+        className="idontknow"
+        to={`/character/${identification}`}
+        onClick={`/character/${identification}`}
+      >
+        <div className="front">
+          <img src={image} alt={name} />
         </div>
-
-    )
-}
+        <div className="back">
+          <div className="details">
+            <h2>
+              {name}
+              <br />
+              <span>{comics}</span>
+            </h2>
+          </div>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
 
 export default Card;
--- src/components/Indexx/Cards/Cards.jsx
+++ src/components/Indexx/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import "./Cards.scss";
 import Card from "./Card";
 
 const Cards = ({ characters }) => {
+  console.log(characters);
   return (
     <div className="cards-container">
       {characters.map(character => {
@@ -13,6 +14,7 @@ const Cards = ({ characters }) => {
               name={character.name}
               comics={character.biography.publisher}
               image={character.image.url}
+              identification={character.id}
             />
           );
         }
